a7a45637903409cc4c177c526a6be1d07aa9762b
S-Y <sy-unimportant@outlook.com>
2022-02-19 14:57:39 +0000

    Added initial onClick events. Needs work

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -3,23 +3,37 @@ import createTodo from './modules/CreateTodo';
 
 function App() {
 
-const [getTodo, setTodo] = useState()
+    let todos = [];
 
-const handleChange = e => {
-setTodo(e.target.value)
-}
+  const [getTodo, setTodo] = useState("")
 
-// on submit, getTodo is sent to create todo, then getTodo is set to "" to reset.
+  const handleChange = e => {
+  setTodo(e.target.value)
+  }
 
 
-// Need an array of tasks. Create factory function for tasks
+  function submitTodo() {
+    let newObj = createTodo(getTodo);
+    todos.push(newObj);
+    console.log(todos);
+    console.log(getTodo);
+    setTodo(""); // Doesn't clear field. Needs work.
+    console.log(getTodo);
 
-  return (
-      <>
-        <input type="text" placeholder="Create task" value={getTodo} onChange={handleChange}/>
-        <h3>{getTodo}</h3>
-      </>
-  );
+  }
+
+  // on submit, getTodo is sent to create todo, then getTodo is set to "" to reset.
+
+
+  // Need an array of tasks. Create factory function for tasks
+
+    return (
+        <>
+          <input type="text" placeholder="Create task" value={getTodo} onChange={handleChange}/>
+          <button onClick={() => submitTodo()}>Create Task</button>
+          <h3>{getTodo}</h3>
+        </>
+    );
 }
 
 export default App;
--- todo-list/src/modules/RenderTodos.js
+++ todo-list/src/modules/RenderTodos.js
@@ -1,3 +1,5 @@
 function renderTodos(arr){
     // loop through each array element and render a div for them.
+
+    
 }
